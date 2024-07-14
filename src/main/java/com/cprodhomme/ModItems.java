06dc0f1466788d82b443bf49ace1b01ed9624676
Clément Prod'homme <clement.prod-homme@captive.fr>
2024-07-14 14:56:11 +0200

    Add Katana

--- src/main/java/com/cprodhomme/ModItems.java
+++ src/main/java/com/cprodhomme/ModItems.java
@@ -4,6 +4,8 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterials;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -14,6 +16,12 @@ public class ModItems {
   public static final Item NINJA_LEGGINGS = register(new ArmorItem(ModArmorMaterials.NINJA, ArmorItem.Type.LEGGINGS, new Item.Settings().maxDamage(ArmorItem.Type.LEGGINGS.getMaxDamage(5))), "ninja_leggings");
   public static final Item NINJA_BOOTS = register(new ArmorItem(ModArmorMaterials.NINJA, ArmorItem.Type.BOOTS, new Item.Settings().maxDamage(ArmorItem.Type.BOOTS.getMaxDamage(5))), "ninja_boots");
 
+  public static final Item WOODEN_KATANA = register(new SwordItem(ToolMaterials.WOOD, new Item.Settings().attributeModifiers(SwordItem.createAttributeModifiers(ToolMaterials.WOOD, 3, -2.4F))), "wooden_katana");
+  public static final Item STONE_KATANA = register(new SwordItem(ToolMaterials.STONE, new Item.Settings().attributeModifiers(SwordItem.createAttributeModifiers(ToolMaterials.STONE, 3, -2.4F))), "stone_katana");
+  public static final Item GOLDEN_KATANA = register(new SwordItem(ToolMaterials.GOLD, new Item.Settings().attributeModifiers(SwordItem.createAttributeModifiers(ToolMaterials.GOLD, 3, -2.4F))), "golden_katana");
+  public static final Item IRON_KATANA = register(new SwordItem(ToolMaterials.IRON, new Item.Settings().attributeModifiers(SwordItem.createAttributeModifiers(ToolMaterials.IRON, 3, -2.4F))), "iron_katana");
+  public static final Item DIAMOND_KATANA = register(new SwordItem(ToolMaterials.DIAMOND, new Item.Settings().attributeModifiers(SwordItem.createAttributeModifiers(ToolMaterials.DIAMOND, 3, -2.4F))), "diamond_katana");
+
   public static void initialize() {
     ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
 		  .register((itemGroup) -> itemGroup.add(ModItems.NINJA_HELMET));
@@ -23,6 +31,17 @@ public class ModItems {
 		  .register((itemGroup) -> itemGroup.add(ModItems.NINJA_LEGGINGS));
     ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
       .register((itemGroup) -> itemGroup.add(ModItems.NINJA_BOOTS));
+
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
+      .register((itemGroup) -> itemGroup.add(ModItems.WOODEN_KATANA));
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
+      .register((itemGroup) -> itemGroup.add(ModItems.STONE_KATANA));
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
+      .register((itemGroup) -> itemGroup.add(ModItems.GOLDEN_KATANA));
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
+      .register((itemGroup) -> itemGroup.add(ModItems.IRON_KATANA));
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
+      .register((itemGroup) -> itemGroup.add(ModItems.DIAMOND_KATANA));
   }
 
 	public static Item register(Item item, String id) {
